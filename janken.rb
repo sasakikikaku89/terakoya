0d43335325ead36c32cb34bc77c9bf67c923db74
伊藤　統彦 <itoh_motohiko@itoutouhikonoMacBook-Pro.local>
2019-07-18 10:51:55 +0900

    じゃんけんゲームの修正_例外処理などの追加

--- janken.rb
+++ janken.rb
@@ -1,9 +1,10 @@
 def hantei(mine,yours)
     if (mine == 1 && yours == 2)||(mine == 2 && yours == 3)||(mine == 3 && yours == 1)
-        puts "YOU LOOOOOOOSE"
-    elsif mine == yours
-    else
         puts "YOU WIIIIIIIIN"
+    elsif mine == yours
+        puts "アイコで　しょ！！"
+    else
+        puts "YOU LOOOOOOOSE"
     end 
 end
 
@@ -12,15 +13,18 @@ puts "最初はぐー。じゃんけん"
 puts "数字を選べ >>>>"
 
 array = ["グー", "チョキ", "パー"]
-array.each_with_index do |each_array,i|
-    puts "#{each_array}: #{i+1}"
+array.each.with_index(1) do |each_array,i|
+    puts "#{each_array}: #{i}"
 end
 
- loop do
+loop do
     mine = gets.chomp.to_i
     yours = [1,2,3].sample
-    puts "あなたは#{array[yours-1]}、私は#{array[mine-1]}"
-    hantei(mine,yours)
+    if mine <= 3 && 0 < mine
+        puts "あなたは#{array[mine-1]}、私は#{array[yours-1]}"
+        hantei(mine,yours)
+    else
+        raise "指定した数字を選択してください！"
+    end
     break if mine != yours
-    puts "アイコで　しょ！！"
 end
